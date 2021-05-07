a1036f880baa5e2cecab8b192d5cf9eb9b28d468
Deveshwar-Saste-9001 <83347492+Deveshwar-Saste-9001@users.noreply.github.com>
2021-05-07 19:16:24 +0530

    Add files via upload

---
+++ Assignment/Asq18.java
@@ -0,0 +1,14 @@
+// 18. Write a Java program to multiply two binary numbers.
+// Input Data:
+// Input the first binary number: 10
+// Input the second binary number: 11
+// Expected Output
+// Product of two binary numbers: 110
+import java.util.*;
+public class Asq18 {
+
+  public static void main(String[] args) {
+   
+    }
+    
+}
